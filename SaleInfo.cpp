772d39905d6ddc1a1b348523a2ce041b6b7990c3
Beni1061 <150958491+Beni1061@users.noreply.github.com>
2023-11-27 12:02:01 -0700

    Add files via upload
    Created printDaySales

---
+++ SaleInfo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "SaleInfo.h"
+using namespace std;
+
+
+void printDaySales() {
+	setSalesByDay(int daySales);
+	cout << "Here are the sales for the day: " << salesByDay << endl;
+}
